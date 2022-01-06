7f8c13183525511d36c1e543d58519cc61cc9f29
Sean <76791097+LordOfBeans@users.noreply.github.com>
2022-01-06 15:29:34 -0500

    Improved Scraping Tool
    publicScraper.py uses a much simpler scraping method that can request an arbitrary number of stories, currently going back 400 stories. main.py could use some improvement.

--- publicScrape.py
+++ publicScrape.py
@@ -1,40 +1,63 @@
-import urllib.request
+import requests
 from bs4 import BeautifulSoup
+import json
 
-#Returns the webpage HTML parsed as a BeautifulSoup object
-def getSoup(url):
-	req = urllib.request.Request(url)
-	with urllib.request.urlopen(req) as response:
-		soup = BeautifulSoup(response.read(), 'html.parser')
-	return soup
+STORY_NUM = 400
+
+param_dict = {
+	"className":"is-style-borders",
+	"moreButton":0,
+	"moreButtonText":"LOAD MORE STORIES",
+	"showAvatar":0,
+	"showCategory":1,
+	"postsToShow":STORY_NUM,
+	"mediaPosition":"right",
+	"":"","":"","":"","":"","":"","":"",
+	"typeScale":3,
+	"imageScale":1,
+	"postType[0]":"post",
+	"showExcerpt":1,
+	"excerptLength":55,
+	"showReadMore":0,
+	"readMoreLabel":"Keep reading",
+	"showDate":1,
+	"showImage":0,
+	"showCaption":0,
+	"disableImageLazyLoad":0,
+	"imageShape":"landscape",
+	"minHeight":0,
+	"showAuthor":1,
+	"postLayout":"list",
+	"columns":3,
+	"mobileStack":0,
+	"sectionHeader":"",
+	"specificMode":0,
+	"textColor":"",
+	"customTextColor":"",
+	"singleMode":0,
+	"showSubtitle":"0",
+	"textAlign":"left",
+	"page":1,
+	"amp":1,
+	"exlude_ids":""
+}
 
 def getStoriesInfo():
-	stories = []
-	soup = getSoup('https://www.publicsource.org/')
-	main_div = soup.find('div',{'class':'entry-content'})
-	for article in main_div.find_all('article'):
-		meta = article.find('div',{'class':'entry-meta'})
+	stories_info = []
+	with requests.get("https://www.publicsource.org/wp-json/newspack-blocks/v1/articles", params = param_dict) as r:
+		stories_json = json.loads(r.text)['items']
+	for story in stories_json:
+		soup = BeautifulSoup(story['html'], 'html.parser')
+		link = soup.find(True, {'class':'entry-title'}).a
+		title = link.string;
+		url = link['href']
+		meta = soup.find('div',{'class':'entry-meta'})
+		author = meta.find('a',{'class':'url'}).string
+		published = meta.time.string
 		try:
-			link = article.find(True,{'class':'entry-title'}).a
-			title = link.string
-			url = link['href']
-			desc = getDesc(url)
+			desc = soup.p.string
 		except AttributeError:
-			title = ''
-			url = ''
-		try:
-			author = meta.find('a',{'class':'url'}).string
-		except AttributeError:
-			author = ''
-		try:
-			published = meta.time.string
-		except AttributeError:
-			published = ''
-		story = ['',title, author, published, desc, url]
-		stories.append(story)
-	return stories
-
-def getDesc(url):
-	soup = getSoup(url)
-	return soup.find('meta',{'property':'og:title'})['content']
-		
+			desc=""
+		story_info = ['', title, author, published, desc, url]
+		stories_info.append(story_info)
+	return stories_info
